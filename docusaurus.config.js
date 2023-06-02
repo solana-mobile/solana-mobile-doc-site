f75c323736f575d8bfc2a804a2d9bd086271a68d
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-06-02 14:55:35 -0700

    Add an entry to gitignore, update docusaurus to have light/dark toggle

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
       image: 'img/solana-mobile-stack-social-card.png',
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: true,
+        disableSwitch: false,
         respectPrefersColorScheme: false,
       },
       navbar: {
