cef39911373baf3022a041bca1d11f9051a9e312
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-06-06 14:36:42 -0700

    Add initial set of pages copied over from publishing tools repo

--- sidebars.js
+++ sidebars.js
@@ -111,6 +111,48 @@ const sidebars = {
     },
     {
       type: 'category',
+      label: 'dApp Store Publishing',
+      collapsible: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'dapp-publishing/intro',
+          label: 'Introduction'
+        },
+        {
+          type: 'doc',
+          id: 'dapp-publishing/setup',
+          label: 'Publishing Tools Setup'
+        },
+        {
+          type: 'doc',
+          id: 'dapp-publishing/publishing',
+          label: 'Publishing to the dApp Store'
+        },
+        {
+          type: 'doc',
+          id: 'dapp-publishing/about',
+          label: 'Why Publish on the dApp Store?'
+        },
+        {
+          type: 'doc',
+          id: 'dapp-publishing/policy',
+          label: 'Publisher Policy'
+        },
+        {
+          type: 'link',
+          href: 'https://github.com/solana-mobile/dapp-publishing/blob/main/docs/DEVELOPER-AGREEMENT.pdf',
+          label: 'Developer Agreement'
+        },
+        {
+          type: 'link',
+          href: 'https://github.com/solana-mobile/dapp-publishing/blob/main/docs/TERMS-OF-USE.pdf',
+          label: 'Solana dApp Store TOU'
+        }
+      ]
+    },
+    {
+      type: 'category',
       label: 'Additional SDKs',
       collapsible: true,
       items: [
