bd539bc212dad3aef27be647e42c99e68e2baa75
Marco Martinez <martinez.marco567@gmail.com>
2023-11-03 03:10:33 -0600

    Create solanaKmpSidebar.js

---
+++ sidebars/solanaKmpSidebar.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+module.exports = {
+    unrealSidebar: [
+        {
+            type: 'doc',
+            id: 'solana-kmp/solana-kmp',
+            label: 'Overview'
+        },
+    ]
+  };
