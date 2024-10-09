1fc4d060e07b6e53a80990fa0c78ef0e5079a0f5
michael sulistio <michaelsulistio@gmail.com>
2024-10-09 11:56:01 -0700

    Add stubbed marketing section

---
+++ sidebars/marketingSidebar.js
@@ -0,0 +1,33 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+module.exports = {
+    marketingSidebar: [
+        {
+            type: 'html',
+            value: 'Getting Started',
+            className: 'sidebar-section-header',
+        },
+        {
+            type: 'doc',
+            id: 'marketing/overview',
+            label: 'Marketing Overview'
+        },
+        {
+            type: 'link',
+            href: 'marketing/overview',
+            label: 'Frequently Asked Questions'
+        },
+        {
+            type: 'link',
+            href: 'marketing/overview',
+            label: 'Brand & Marketing Guidelines'
+        },
+        {
+            type: 'link',
+            href: 'marketing/overview',
+            label: 'Post Launch Marketing'
+        },
+
+    ]
+  };
