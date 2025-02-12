98e547802ee12bad578e28bb088ad503f04df410
Michael Sulistio <michaelsulistio@gmail.com>
2025-02-11 16:33:17 -0800

    Replace Seeker Gradient png with linear gradient

--- src/components/SolanaMobileHero/index.tsx
+++ src/components/SolanaMobileHero/index.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { useColorMode } from "@docusaurus/theme-common";
 
 function SolanaMobileHero({ children }) {
-  const { colorMode } = useColorMode();
-
-  const heroBackgroundSrc = "img/SeekerGradient.png";
-
   return (
     <div className={styles.heroContainer}>
       <div
         className={styles.heroBackground}
-        style={
-          colorMode === "dark"
-            ? { backgroundImage: `url(${heroBackgroundSrc})` }
-            : {}
-        }
       />
       <div className={styles.heroContent}>{children}</div>
     </div>
